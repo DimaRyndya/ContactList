a20f75ab94d451d5c6522f45ca4598c0a5a9b77a
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-08-21 20:22:23 +0300

    Initial commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Dmitriy Ryndya on 21.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
